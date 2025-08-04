b5916303dcbdd2c9053cfc9ed48f1c2356f9ff45
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 19:45:15 -0700

    Create PHW_NameGame.py

---
+++ ProgHW/PHW_NameGame.py
@@ -0,0 +1,38 @@
+'''
+
+Programming HW - Name Game
+
+Description: Plays name game lyrics for any name!
+
+Developer Name(s): 
+
+Date: 
+
+'''
+##########################################
+# IMPORTS:
+#   modules needed for program
+##########################################
+
+
+##########################################
+# FUNCTIONS:
+#   functions useful to main program
+##########################################
+#NOTE: your solution code goes inside name_game function below
+def name_game(name):
+	'''Prints one verse of the name game using the given name.'''
+	print("Not working yet! Replace this line of code with your solution :D")
+	
+	
+	
+
+##########################################
+# MAIN PROGRAM:
+#   beginning of running program
+##########################################
+#NOTE: leave the following alone, this will get the user input and call your function!
+verse_name = input("Next name: ")
+name_game(verse_name)
+
+
